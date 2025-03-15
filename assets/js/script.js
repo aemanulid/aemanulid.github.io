8529c088f6e2f37592ca23f4deeeceddbfc59c2c
aemanulid <aemanulid@up.edu.ph>
2025-03-15 19:07:29 +0000

    update comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navMobile = document.querySelector('.nav-mobile');
 
+//Open mobile nav when toggle is clicked
 navToggle.addEventListener('click', () => {
     navMobile.classList.toggle('open');
     if (navMobile.classList.contains('open')) {
@@ -10,14 +11,15 @@ navToggle.addEventListener('click', () => {
     }
 });
 
+//Close mobile nav when clicked elsewhere
 document.addEventListener('click', (event) => {
     if (navMobile.classList.contains('open') && !navMobile.contains(event.target) && event.target !== navToggle) {
         navMobile.classList.remove('open');
-        navMobile.style.right = '-250px';
+        navMobile.style.right = '-250px'; // Slide out of view
     }
 });
 
-
+//Animation for slow scroll after scrollLink is clicked
 const scrollLink = document.getElementById('scrollLink');
 const targetArea = document.getElementById('first');
 
@@ -28,6 +30,7 @@ scrollLink.addEventListener('click', (event) => {
     });
 });
 
+//Fun fact popup animations
 const modal = document.getElementById('funFactModal');
 const btn = document.getElementById('funFactButton');
 const span = document.getElementsByClassName('close')[0];
